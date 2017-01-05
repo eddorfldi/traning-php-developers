42698ecadddcb6dbcb335c2a9b498673cf606b8b
reifaldi <reirifaldi@gmail.com>
2017-01-05 17:24:44 +0700

    Tugas CSS.
    CSS 05/01/17

---
+++ CSS/pertama.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Belajar HTML 5 dan CSS dasar.</title>
+    <link rel="stylesheet" type="text/css" href="pertama.css">
+</head>
+<body>
+    <header>
+        <h1>I like study HTML5 and CSS.</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#">HTML 5</a></li>
+            <li><a href="#">CSS</a></li>
+            <li><a href="#">Rewards</a></li>
+        </ul>
+    </nav>
+    <section>
+        <article>
+            <p class="satu">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </article>
+        <article>
+            <p class="dua">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </article>
+        <article>
+            <p class="tiga">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </article>
+    </section>
+
+    <footer>
+        <p>Copyright 2017 - RIDI.</p>
+    </footer>
+</body>
+</html>
